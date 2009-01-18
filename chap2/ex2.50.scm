61ef25919dfe7436d31fe88ac90bace0dd537881
Drew Hess <dhess-src@bothan.net>
2009-01-17 20:13:44 -0800

    Added ex2.50.scm

---
+++ chap2/ex2.50.scm
@@ -0,0 +1,22 @@
+;;; Requires the PLT DrScheme graphical environment. Set the language
+;;; to "Module".
+#lang scheme
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
+(define (my-flip-horiz painter)
+  ((transform-painter (make-vect 1.0 0.0)
+                      (make-vect 0.0 0.0)
+                      (make-vect 1.0 1.0))
+   painter))
+
+(define (my-rotate180 painter)
+  ((transform-painter (make-vect 1.0 1.0)
+                      (make-vect 0.0 1.0)
+                      (make-vect 1.0 0.0))
+   painter))
+
+(define (my-rotate270 painter)
+  ((transform-painter (make-vect 0.0 1.0)
+                      (make-vect 0.0 0.0)
+                      (make-vect 1.0 1.0))
+   painter))
