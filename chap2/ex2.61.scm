a775f84e8c706884c877f4cf289403f5b6389026
Drew Hess <dhess-src@bothan.net>
2009-05-05 00:56:47 -0700

    ex2.61.scm: removed extraneous definitions.

--- chap2/ex2.61.scm
+++ chap2/ex2.61.scm
@@ -1,13 +1,3 @@
-(define true #t)
-
-(define false #f)
-
-(define (element-of-set? x set)
-  (cond ((null? set) false)
-        ((= x (car set)) true)
-        ((< x (car set)) false)
-        (else (element-of-set? x (cdr set)))))
-
 (define (adjoin-set x set)
   (cond ((null? set) (list x))
         ((= x (car set)) set)
