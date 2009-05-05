39204760b8a08a4fcc06a378c645b3d7214a89fc
Drew Hess <dhess-src@bothan.net>
2009-05-04 22:21:31 -0700

    Added ex2.61.scm.

---
+++ chap2/ex2.61.scm
@@ -0,0 +1,15 @@
+(define true #t)
+
+(define false #f)
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
