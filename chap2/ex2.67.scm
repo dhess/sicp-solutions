94b2e8c0b4dae8588fa900b1f1c0f89367fd30bd
Drew Hess <src@drewhess.com>
2012-07-04 00:35:02 -0700

    Removed some unnecessary code from ex2.67.

--- chap2/ex2.67.scm
+++ chap2/ex2.67.scm
@@ -45,20 +45,6 @@
         ((= bit 1) (right-branch branch)) 
         (else (error "bad bit -- CHOOSE-BRANCH" bit)))) 
 
-(define (adjoin-set x set) 
-  (cond ((null? set) (list x)) 
-        ((< (weight x) (weight (car set))) (cons x set)) 
-        (else (cons (car set) 
-                    (adjoin-set x (cdr set)))))) 
-
-(define (make-leaf-set pairs) 
-  (if (null? pairs) 
-      '() 
-      (let ((pair (car pairs))) 
-        (adjoin-set (make-leaf (car pair)
-                               (cadr pair))
-                    (make-leaf-set (cdr pairs)))))) 
-
 (define sample-tree 
   (make-code-tree (make-leaf 'A 4) 
                   (make-code-tree 
